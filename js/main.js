ca47a96d7c633d4df3f265581ccbbc8a5d319596
Alexey.Krokhalev <Alexey.Krokhalev@urfu.me>
2022-09-20 19:36:49 +0500

    Рефакторинг

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 function getRandInt(intFrom, intTo){
-
-  if (intFrom>intTo || intFrom < 0 || intTo  <0 ){
+  if (intFrom > intTo || intFrom < 0 || intTo < 0 ){
     return  new Error('Error. Change input values');
   }
-
-  return Math.round(intFrom - 0.5+ Math.random(intFrom, intTo) * (1+intTo-intFrom));
+  return Math.round(intFrom - 0.5 + Math.random(intFrom, intTo) * (1 + intTo - intFrom));
 }
-
-//function checkCommentLength(comment, length){
-//      return comment.length <= length;
-//}
 
 function checkStringLength(string, length){
   return string.length <= length;
