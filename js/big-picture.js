4503b42af9b8f6cdb876735f2d65a9ab962d4a56
Alexey.Krokhalev <Alexey.Krokhalev@urfu.me>
2022-11-22 02:07:05 +0500

    7.2

---
+++ js/big-picture.js
@@ -0,0 +1,49 @@
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureCloseButton = bigPicture.querySelector('.big-picture__cancel');
+
+function CloseBigPicture(){
+  bigPicture.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  bigPictureCloseButton.removeEventListener('click', closeBigPicture);
+  document.removeEventListener('keydown', onPopupEscKeydown);
+};
+
+function AddModalCloseHandlers(){
+  bigPicture.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  bigPictureCloseButton.addEventListener('click', CloseBigPicture);
+  document.addEventListener('keydown', onPopupEscKeydown);
+};
+
+function OpenBigPicture(pictureData){
+  AddModalCloseHandlers();
+  bigPicture.querySelector('.big-picture__img').querySelector('img').src = pictureData.url;
+  bigPicture.querySelector('.likes-count').textContent = pictureData.likes;
+  bigPicture.querySelector('.comments-count').textContent = pictureData.comments.length.toString();
+  bigPicture.querySelector('.social__caption').textContent = pictureData.description;
+  const commentsList = bigPicture.querySelector('.social__comments');
+
+  commentsList.innerHTML = '';
+  pictureData.comments.forEach((commentData) => {
+    commentsList.insertAdjacentHTML('beforeend', `
+    <li class="social__comment">
+        <img
+            class="social__picture"
+            src="${commentData.avatar}"
+            alt="${commentData.name}"
+            width="35" height="35">
+        <p class="social__text">${commentData.message}</p>
+    </li>`);
+  });
+  bigPicture.querySelector('.social__comment-count').classList.add('hidden');
+  bigPicture.querySelector('.comments-loader').classList.add('hidden');
+};
+
+function onPopupEscKeydown (evt) {
+  if (evt.key === "Escape") {
+    evt.preventDefault();
+    closeBigPicture();
+  }
+}
+
+export {OpenBigPicture};
--- js/photo rendering.js
+++ js/photo rendering.js
@@ -1,3 +1,4 @@
+import { OpenBigPicture } from './big-picture.js';
 import {createPhotos} from './data.js';
 
 const photoTemplate = document.querySelector('#picture').textContent.querySelector('.picture');
@@ -6,13 +7,25 @@ const photoListFragment = document.createDocumentFragment();
 const photosArray = createPhotos();
 
 photosArray.forEach(element => {
-    const photo = photoTemplate.cloneNode(true);
-    picture.querySelector('.picture__img').src = element.url;
-    picture.querySelector('.picture__comments').textContent = element.comments.length;
-    picture.querySelector('.picture_likes').textContent = element.likes;
+    const picture = photoTemplate.cloneNode(true);
+    const pictureImg = picture.querySelector('.picture_img');
+
+    pictureImg.src = element.url
+    pictureImg.dataset.element = JSON.stringify(element);
+
+    picture.querySelector('.picture__comments').textContent = element.comments.length.toString();
+    picture.querySelector('.picture__likes').textContent = element.likes;
 
     photoListFragment.appendChild(picture);
 
 });
 
 photoList.appendChild(photoListFragment);
+
+photoList.addEventListener('click', (evt) =>{
+  const target = evt.target;
+  if (target.nodeName === 'IMG'){
+    OpenBigPicture(JSON.parse(target.dataset.element))
+  }
+
+})
