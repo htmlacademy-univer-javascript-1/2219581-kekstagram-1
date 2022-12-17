b538208f083bda9796baf69a2fce6d484a3cab7c
Alexey.Krokhalev <Alexey.Krokhalev@urfu.me>
2022-12-17 16:24:41 +0500

    правда или действие 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import {CreatedPhotos} from './data.js';
-import { renderPictures } from './photo-rendering.js';
+import {renderPictures} from './photo-rendering.js';
+import {uploadForm} from './upload-form.js';
 
 const picturesArray = CreatedPhotos();
 renderPictures(picturesArray);
+
+uploadForm();
