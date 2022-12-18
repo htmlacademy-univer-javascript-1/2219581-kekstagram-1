25f0af638f63212f5b169e6f56881141b14f0e0b
Keks <keks@htmlacademy.ru>
2022-12-18 21:27:37 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/connect-server.js
@@ -0,0 +1,21 @@
+  // eslint-disable-next-line no-unused-vars
+const URL = {
+    'GET': 'https://26.javascript.pages.academy/kekstagram/data',
+    'POST': 'https://26.javascript.pages.academy/kekstagram'
+  };
+
+const setData = (onSuccess, onFail, method, body) => {
+    fetch(URL[method], {
+      method: method,
+      body
+    })
+    .then((response) => response.json())
+    .then((pictures) => {
+        onSuccess(pictures);
+    })
+    .catch((message) => {
+        onFail(message);
+    });
+};
+
+  export {setData};
---
+++ 13/js/util.js
@@ -0,0 +1,38 @@
+function getRandInt(intFrom, intTo){
+  if (intFrom > intTo || intFrom < 0 || intTo < 0 ){
+    return  new Error('Error. Change input values');
+  }
+  return Math.round(intFrom - 0.5 + Math.random(intFrom, intTo) * (1 + intTo - intFrom));
+}
+
+const generateArray = (length, max) => (
+  [...new Array(length)].map(() => Math.round(Math.random() * max)));
+
+function getId (usersId) {
+  const temp = usersId[getRandInt(0,usersId.length-1)];
+
+  delete(usersId[getRandInt(0,usersId.length-1)]);
+  return temp;
+}
+
+const pressEscape = (evt) => evt.key === 'Escape';
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+const throttle = (array) => {
+  for(let firstIndex = array.length - 1; firstIndex > 0; firstIndex--) {
+    const randomIndex = Math.floor(Math.random() * (firstIndex + 1));
+    [array[firstIndex], array[randomIndex]] = [array[randomIndex], array[firstIndex]];
+  }
+
+  return array;
+};
+
+export {getRandInt, generateArray, getId, pressEscape, debounce, throttle};
